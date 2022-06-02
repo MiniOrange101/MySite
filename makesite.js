669150aa6592bff2d9872838dc98ea1b65614969
master <miaomiaoli@MiniOrangeMBP.local>
2022-06-02 10:02:04 +0800

    JS

---
+++ makesite.js
@@ -0,0 +1,15 @@
+function ElemObj(tag, attribute, text) {
+    this.tag = tag;
+    this.attribute = attribute;
+    this.child = child;
+    var ele = document.createElement(tag);
+    var i;
+    for (i in attribute) {
+        ele.setAttribute(i, attribute[i]);
+    }
+    ele.appendChild(document.createTextNode(text));
+    this.element = ele;
+}
+function init() {
+
+}
