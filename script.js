ffd0666f156e711889c210de0d08dd9bafac7583
master <miaomiaoli@MiniOrangeMBP.local>
2022-06-07 07:53:30 +0800

    NOfoundation

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+document.write("<link rel=\"stylesheet\" href=\"http:\/\/cdn.static.runoob.com\/libs\/foundation\/5.5.3\/css\/foundation.min.css\">");
+document.write("<script src=\"http:\/\/cdn.static.runoob.com\/libs\/jquery\/2.1.1\/jquery.min.js\"><\/script>");
+document.write("<script src=\"http:\/\/cdn.static.runoob.com\/libs\/foundation\/5.5.3\/js\/foundation.min.js\"><\/script>");
+document.write("<script src=\"http:\/\/cdn.static.runoob.com\/libs\/foundation\/5.5.3\/js\/vendor\/modernizr.js\"><\/script>");
 document.write("<link rel=\"stylesheet\" href=\"https:\/\/cdn.staticfile.org\/font-awesome\/4.7.0\/css\/font-awesome.css\">");
 //foundation
 document.write("<link rel=\"stylesheet\" href=\"http:\/\/static.runoob.com\/assets\/foundation-icons\/foundation-icons.css\">");
