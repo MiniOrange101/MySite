b3b7caa7b989486c920de935d5b614a03de3c8f1
master <miaomiaoli@MiniOrangeMBP.local>
2022-06-06 11:50:46 +0800

    ADD

--- makesite.js
+++ makesite.js
@@ -1,5 +1,7 @@
 var Elements = new Object();//定义元素对象容器
 var i = 0;
+var j = 0;
+var k = 0;
 class ElemObj {
     constructor(id, tag, attribute, style, value) {
         this.tag = tag;
@@ -68,16 +70,130 @@ function refresh() {
 
 function addElement(id, tag, value) {
     if (id == "") {
-        document.getElementById("piderr").style.display = "block";
+        document.getElementById(tag + "iderr").style.display = "block";
     }
     if (id != "") {
-        document.getElementById("piderr").style.display = "none";
-        Elements[id] = new ElemObj(id, tag, {}, {}, value);
+        document.getElementById(tag + "iderr").style.display = "none";
+        switch (tag) {
+            case "p":
+                Elements[id] = new ElemObj(id, tag, {}, {}, value);
+                break;
+            case "img":
+                Elements[id] = new ElemObj(id, tag, { src: value.src }, { width: value.width, height: value.height }, "");
+                break;
+            case "table":
+                Elements[id] = new ElemObj(id, tag, {}, {}, "");
+                Elements[id].element.appendChild(constructTable(value));
+                break;
+            case "list":
+                Elements[id] = new ElemObj(id, value[0], {}, {}, "");
+                for (i in value.slice(1)) {
+                    Elements[id].element.appendChild(document.createElement("li").appendChild(document.createTextNode(value.slice(1)[i])));
+                }
+                break;
+            case "form":
+                Elements[id] = new ElemObj(id, tag, {}, {}, "");
+                break;
+            case "iframe":
+                Elements[id] = new ElemObj(id, tag, { src: value[2] }, { width: value[0], height: value[1] }, "");
+                break;
+            case "script":
+                if (value[0]) {
+                    Elements[id] = new ElemObj(id, tag, { src: value[1] }, {}, "");
+                } else if (!value[0]) {
+                    Elements[id] = new ElemObj(id, tag, {}, {}, value[1]);
+                } else {
+                    return 0;
+                }
+        }
         document.getElementById("container").appendChild(Elements[id].element);
-        $("div#askforpinfo.reveal-modal a.close-reveal-modal").trigger("click");
-        document.getElementById("Pid").value = "";
-        document.getElementById("Pcontent").value = "";
+        $("div#askfor" + tag + "info.reveal-modal a.close-reveal-modal").trigger("click");
+        document.getElementById(tag + "id").value = "";
+        document.getElementById(tag + "content").value = "";
+        makelLi(tag, id);
     } else {
         return 0;
     }
+}
+
+function makelLi(tag, id) {
+    var shownText;
+    var isform = 0;
+    switch (tag) {
+        case "p":
+            shownText = "文本";
+            break;
+        case "img":
+            shownText = "图片";
+            break;
+        case "table":
+            shownText = "表格";
+            break;
+        case "ol":
+            shownText = "有序列表";
+            break;
+        case "ul":
+            shownText = "无序列表";
+            break;
+        case "form":
+            shownText = "表单";
+            break;
+        case "form input.text":
+            shownText = "文本输入框";
+            break;
+        case "form input.password":
+            shownText = "密码输入框";
+            break;
+        case "form input.submit":
+            shownText = "提交按钮";
+            break;
+        case "form input.other":
+            shownText = "获取输入框";
+            break;
+        case "form other":
+            shownText = "表单获取框";
+            break;
+        case "iframe":
+            shownText = "框架";
+            break;
+        case "script":
+            shownText = "JS脚本";
+            break;
+        default:
+            shownText = "其他元素";
+            break;
+    }
+    var e_0 = document.createElement("li");
+    var e_1 = document.createElement("a");
+    e_1.setAttribute("href", "javascript:ElementSelected(" + id + ");");
+    e_1.appendChild(document.createTextNode(shownText));
+    var e_2 = document.createElement("strong");
+    e_2.appendChild(document.createTextNode(" id=" + id));
+    e_1.appendChild(e_2);
+    e_0.appendChild(e_1);
+    document.getElementById("ElementsList").appendChild(e_0);
+}
+
+function ElementSelected(id) {
+    Elements[id].setHighlight();
+}
+
+function constructTable(value) {
+    var table = document.createElement("tbody");
+    for (i in value) {
+        var tr = document.createElement("tr");
+        for (j in value[i]) {
+            tr.appendChild(document.createElement("td").appendChild(document.createTextNode(value[i][j])));
+        }
+        table.appendChild(tr);
+    }
+    return table;
+}
+
+function constructList(value) {
+    var list = document.createElement(value[0]);
+    for (i in value.slice(1)) {
+        list.appendChild(document.createElement("li").appendChild(document.createTextNode(value.slice(1)[i])));
+    }
+    return list;
 }
